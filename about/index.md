06ded5e1348582fa00ef74e9972fc1a0c3d634aa
zll17 <zhangleilan@gmail.com>
2022-06-29 20:40:55 +0800

    Site updated: 2022-06-29 20:40:54

--- about/index.md
+++
@@ -1,7 +0,0 @@
----
-title: About
-date: 2018-12-03 16:50:32
-layout: about
----
-
-> A lot can happen between now and never.
---
+++ assets/configure_vim.sh
@@ -0,0 +1,7 @@
+mkdir .vim
+cd .vim
+mkdir bundle
+git clone https://github.com.cnpmjs.org/VundleVim/Vundle.vim.git
+cd ~/
+wget https://zll17.github.io/about/vimrc
+mv vimrc .vimrc
