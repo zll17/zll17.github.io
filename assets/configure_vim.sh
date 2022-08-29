13daf78d86b963b886fa93bae56a1e1798532de0
zll17 <zhangleilan@gmail.com>
2022-08-29 12:41:32 +0800

    Site updated: 2022-08-29 12:41:30

--- assets/configure_vim.sh
+++ assets/configure_vim.sh
@@ -3,5 +3,5 @@ cd .vim
 mkdir bundle
 git clone https://github.com.cnpmjs.org/VundleVim/Vundle.vim.git
 cd ~/
-wget https://zll17.github.io/about/vimrc
+wget https://zll17.github.io/assets/vimrc
 mv vimrc .vimrc
